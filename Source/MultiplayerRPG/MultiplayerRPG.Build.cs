ec0871dcdeadb11ecad7280241bb082d65882a87
Yo714 <khaifengtay@gmail.com>
2024-11-27 11:33:14 +0800

    Initial commit

---
+++ Source/MultiplayerRPG/MultiplayerRPG.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class MultiplayerRPG : ModuleRules
+{
+	public MultiplayerRPG(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+	}
+}
---
+++ Source/MultiplayerRPG/MultiplayerRPGGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MultiplayerRPGGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMultiplayerRPGGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMultiplayerRPGGameMode();
+};
+
+
+
